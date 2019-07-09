a673bba233563cfa0cbfd3056f3c7cad89a2773b
v1c7or20 <victor.pena@utec.edu.pe>
2019-07-09 12:22:16 -0500

    register completed
    falta el recicler view(main)
    falta productos, tienda (fragments)
    tenais que complicarlo con fragmentes no??

--- app/src/main/java/cs2901/utec/chat_mobile/RegistroActivity.java
+++ app/src/main/java/cs2901/utec/chat_mobile/RegistroActivity.java
@@ -1,15 +1,15 @@
 package cs2901.utec.chat_mobile;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.ComponentCallbacks;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Map;
+import java.util.HashMap;
+import org.json.JSONObject;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,11 +18,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import android.content.Intent;
+import org.json.JSONException;
+import android.view.View;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -37,6 +35,10 @@ public class RegistroActivity extends AppCompatActivity {
         return this;
     }
 
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
     public void onBtnLoginClicked(View view) {
         // 1. Getting username and password inputs from view
         EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
@@ -46,15 +48,15 @@ public class RegistroActivity extends AppCompatActivity {
 
         String username = txtUsername.getText().toString();
         String password = txtPassword.getText().toString();
-        String nombre = txtNombre.getText().toString();
+        final String nombre = txtNombre.getText().toString();
         String apellido = txtApellido.getText().toString();
 
         // 2. Creating a message from user input data
         Map<String, String> message = new HashMap<>();
         message.put("username", username);
         message.put("password", password);
-        message.put("nombre", nombre);
-        message.put("apellido", apellido);
+        message.put("name", nombre);
+        message.put("fullname", apellido);
 
         // 3. Converting the message object to JSON string (jsonify)
         JSONObject jsonMessage = new JSONObject(message);
@@ -62,25 +64,22 @@ public class RegistroActivity extends AppCompatActivity {
         // 4. Sending json message to Server
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.POST,
-                "http://10.0.2.2:5000/register",
+                "http://10.0.2.2:5000/users",
                 jsonMessage,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         //TODO
                         try {
-                            String message = response.getString("message");
-                            if(message.equals("Authorized")) {
-                                showMessage("Authenticated");
-                                EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
-                                String username = txtUsername.getText().toString();
+                            String message = response.getString("user");
+                            if(message.equals("created")) {
+                                showMessage("Created");
                                 Intent intent = new Intent(getActivity(), MainActivity.class);
-                                intent.putExtra("user_id", response.getInt("user_id"));
-                                intent.putExtra("username", username);
+                                intent.putExtra("username", nombre);
                                 startActivity(intent);
                             }
                             else {
-                                showMessage("Wrong username or password");
+                                showMessage("Server error");
                             }
                             showMessage(response.toString());
                         }catch (Exception e) {
@@ -94,7 +93,7 @@ public class RegistroActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         error.printStackTrace();
                         if( error instanceof AuthFailureError){
-                            showMessage("Unauthorized");
+                            showMessage("Try again");
                         }
                         else {
                             showMessage(error.getMessage());
